deaa37298eed3a2127c6b6226738de2f2bb5d7fd
Måns Magnusson <mons.magnusson@gmail.com>
2014-10-30 10:38:01 +0100

    set_assignment shell

---
+++ R/set_assignment.R
@@ -0,0 +1,70 @@
+#' @title
+#' Set assignment to mark
+#' 
+#' @details
+#' Sets the assignment to mark. Behind the scenes it download the test suite for the assignment.
+#' 
+#' @param path
+#' Path to the yml file
+#' @param auth_token 
+#' Authorization token (for github)
+#' 
+#' @export
+check_assignment_yml <- function(){}
+
+# Get the yml file and download the test suite and store in a temp folder.
+
+
+#' @title
+#' Check yml file to be correct.
+#' 
+#' @details
+#' Check the yml file that it is correct and can be used. Otherwise warn.
+#' 
+#' @return
+#' boolean 
+#' 
+check_assignment_yml <- function(){}
+
+
+#' @title
+#' Get the path type.
+#' 
+#' @details
+#' Check the path type. 
+#' 
+#' @return
+#' path object
+#' 
+path_type <- function(){}
+
+
+
+
+#' @title
+#' Get the file from the path
+#' 
+#' @details
+#' Get the file from the path.
+#' 
+#' @param path
+#' The path to get the file from
+#' @param dest
+#' Destination for the file
+#' 
+#' @return
+#' 
+get_file <- function(){}
+
+
+
+#' @title
+#' Load assignment information
+#' 
+#' @details
+#' Check if there is an assignmentfile and load it.
+#' 
+#' @return
+#' Assignment object.
+#' 
+load_assignment_info <- function(){}
