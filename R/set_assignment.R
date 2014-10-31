19be70548772dac650ad04fd707658e976ce47eb
Måns Magnusson <mons.magnusson@gmail.com>
2014-10-31 17:50:39 +0100

    Set assignment (almost) done

--- R/set_assignment.R
+++ R/set_assignment.R
@@ -11,16 +11,24 @@
 #' 
 #' @examples
 #' set_assignment(path = "https://raw.githubusercontent.com/MansMeg/KursRprgm/master/Labs/Test/mandatory.R")
+#' set_assignment(path = "https://raw.githubusercontent.com/MansMeg/KursRprgm/master/Labs/Test/d1.yml")
 #' 
 #' @export
 set_assignment <- function(path, auth_token = NULL){
   path <- path_type(path)
-  if(inherits(path,what = "path_error")) stop("Path does not exist", call. = FALSE)
+  if(inherits(path,what = "path_error")) stop("Path/url does not work.")
   temp_folder_check_create()
-  dest <- paste0(tempdir(), "/markmyassignment/assignment.yml")
+  temp_file <- tempfile()
+  on.exit(unlink(temp_file))
+  dest <- paste0(tempdir(), "/markmyassignment/assignment1.yml")
+  get_file(path, temp_file)  
+  if(!assignment_yml_ok(path = temp_file)) {
+    stop("Not a correct assignment file.")
+  }
   if(file.exists(dest)) file.remove(dest)
-  get_file(path, dest)  
-  if(!check_assignment_yml()) file.remove(dest)
+  file.rename(from = temp_file, to = dest)
+  assignment <- read_assignment_yml()
+  message("Assignment set:\n", assignment$name, " : ", assignment$description)
   invisible(dest)
 }
 
@@ -37,7 +45,7 @@ temp_folder_check_create <- function() {
 }
 
 #' @title
-#'   Check yml file to be correct.
+#'   Check the yml file to be a correct assignment yml.
 #' 
 #' @details
 #'   Check the yml file that it is correct and can be used. Otherwise warn.
@@ -45,8 +53,13 @@ temp_folder_check_create <- function() {
 #' @return 
 #'   boolean 
 #' 
-check_assignment_yml <- function(){
-  assignment <- read_assignment_yml()
+assignment_yml_ok <- function(path = NULL){
+  assignment <- try(read_assignment_yml(path), silent = TRUE)
+  if(inherits(assignment, "try-error")) return(FALSE)
+  
+  # Check the structure here!
+  
+  TRUE
 }
 
 
@@ -87,11 +100,23 @@ path_type <- function(path){
 #' @param dest
 #'  Destination for the file
 #' 
+#' @examples
+#'   get_file(path, dest)
 #' 
-get_file <- function(path, dest){
+get_file <- function(path, dest, ...){
   stopifnot(!inherits(path, "path_error"))
+  UseMethod("get_file")
 }
 
+get_file.path_local <- function(path, dest, ...){
+  file.copy(from = path, to = dest, overwrite = TRUE)
+}
+
+get_file.path_http <- function(path, dest, ...){
+  request <- httr::GET(path, ...)
+  httr::stop_for_status(request)
+  writeBin(httr::content(request, "raw"), dest)
+}
 
 #' @title
 #' Load assignment information
@@ -101,18 +126,31 @@ get_file <- function(path, dest){
 #' 
 #' @return assignment object
 #' 
-read_assignment_yml <- function(){
-  assignment_file <- paste0(tempdir(),"/markmyassignment/assignment.yml")
+#' @examples
+#'  read_assignment_yml()
+read_assignment_yml <- function(path = NULL){
+  if(is.null(path)){
+    assignment_file <- paste0(tempdir(),"/markmyassignment/assignment1.yml")
+  } else {
+    assignment_file <- path
+  }
   if(file.exists(assignment_file)){
-    return(yaml::yaml.load_file(assignment_file))
+    res <- yaml::yaml.load_file(assignment_file)
+    if(is.list(res)) return(res) else stop("Not a correct .yml file")
   } else {
     stop("No assignment has been set. Please use set_assignment().", call. = FALSE)
   }
 }
 
+
 #' @title
-#'   Show the tasks in the assignment.
+#'   Get the name of the tasks in the assignment.
 #' 
+#' @examples
+#'  show_tasks()
 #' 
 #' @export
-show_tasks <- function(){}
+show_tasks <- function(){
+  assignment <- read_assignment_yml()
+  names(assignment$task)
+}
