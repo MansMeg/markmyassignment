30cb0fade0774abcc31bd2183f8cc9423c8c987d
oscpe436 <oscpe436@student.liu.se>
2015-11-01 15:37:37 +0100

    Bug fix in test suite / update to conform with updated version of testthat.

--- tests/testthat/test-mark_my_assignment.R
+++ tests/testthat/test-mark_my_assignment.R
@@ -5,7 +5,7 @@ test_that(desc="mark_my_assignment()",{
   suppressMessages(set_assignment(paste0(system.file(package = "markmyassignment"), "/extdata/example_assignment01.yml")))
   
   if(length(ls(name = .GlobalEnv)) == 0){
-    expect_is(mark_my_assignment(mark_file = paste0(system.file(package = "markmyassignment"), "/extdata/example_lab_file.R"), quiet = TRUE), "data.frame")  
+    expect_is(mark_my_assignment(mark_file = paste0(system.file(package = "markmyassignment"), "/extdata/example_lab_file.R"), quiet = TRUE), "testthat_results")  
     expect_error(mark_my_assignment(mark_file = paste0(system.file(package = "markmyassignment"), "/extdata/example_lab_file_circular.R"), quiet = TRUE))
   }
   
@@ -13,12 +13,12 @@ test_that(desc="mark_my_assignment()",{
   
   expect_is(capture.output(mark_my_assignment()), "character")
   expect_equal(capture.output(mark_my_assignment())[1], "Marking assignment...")
-  expect_is(mark_my_assignment(quiet = TRUE), "data.frame")
+  expect_is(mark_my_assignment(quiet = TRUE), "testthat_results")
   expect_true(sum(grepl(x = capture.output(mark_my_assignment(tasks = "task1")), pattern = "Marking assignment..."))==1)
-  expect_is(mark_my_assignment(tasks = "task1", quiet = TRUE), "data.frame")
-  expect_equal(nrow(mark_my_assignment(tasks = "task1", quiet = TRUE)), 2)
-  expect_is(mark_my_assignment(tasks = c("task1", "task2"), quiet = TRUE), "data.frame")
-  expect_is(mark_my_assignment(force_get_tests = TRUE, quiet = TRUE), "data.frame")
+  expect_is(mark_my_assignment(tasks = "task1", quiet = TRUE), "testthat_results")
+  expect_equal(length(mark_my_assignment(tasks = "task1", quiet = TRUE)), 2)
+  expect_is(mark_my_assignment(tasks = c("task1", "task2"), quiet = TRUE), "testthat_results")
+  expect_is(mark_my_assignment(force_get_tests = TRUE, quiet = TRUE), "testthat_results")
   expect_error(mark_my_assignment(mark_file = paste0(system.file(package = "markmyassignment"), "/extdata/example_lab_file.R"), quiet = TRUE))
 
 })
