9f2ca621ac89567f7939fa4f6b686927cf3aa7ca
oscpe436 <oscpe436@student.liu.se>
2015-12-30 12:08:18 +0100

    Added check_installed_packages() for set_assignment and a few tests.

--- tests/testthat/test-set_assignment.R
+++ tests/testthat/test-set_assignment.R
@@ -37,3 +37,12 @@ test_that(desc="show_tasks()",{
   suppressMessages(set_assignment(correct_local2))
   expect_equal(show_tasks(), c("task1","task2"))
 })
+
+test_that(desc="check_installed_packages()",{
+  assgn_path <- paste0(system.file(package = "markmyassignment"), "/extdata/example_assignment07_pkgs.yml")
+  expect_warning(set_assignment(path = assgn_path))
+  if( all(c("ggplot2", "stringr") %in% installed.packages()) ){
+    library(ggplot2) ; library(stringr)
+    expect_is(suppressMessages(set_assignment(assgn_path)), "character")
+  }
+})
