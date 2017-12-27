5e0b74fe2e369a163dd7a1dee7b9ffcbcd1d1831
Måns Magnusson <mons.magnusson@gmail.com>
2017-12-27 16:25:21 +0100

    Fixed buggs

--- R/mark_my_file.R
+++ R/mark_my_file.R
@@ -15,7 +15,7 @@
 #'   Force download of test files before marking of assignments. Default is FALSE.
 #' @param quiet
 #'   Should test be run without output?
-#' @param reporter to use. Default is the 'summary' or specified in assignment yml file.
+#' @param ... further arguments sent to \code{test_dir()}.
 #' 
 #' @examples
 #' assignment_path <- 
@@ -24,24 +24,23 @@
 #' mark_my_file(mark_file = file_path, assignment_path = assignment_path)
 #' 
 #' @export
-mark_my_file <- function(tasks = NULL, mark_file=file.choose(), assignment_path, force_get_tests = FALSE, quiet = FALSE, reporter){
+mark_my_file <- function(tasks = NULL, mark_file=file.choose(), assignment_path = NULL, force_get_tests = FALSE, quiet = FALSE, ...){
+  checkmate::assert_character(tasks, null.ok = TRUE)
+  checkmate::assert_file_exists(mark_file)
+  if(!is.null(assignment_path)) checkmate::assert_file_exists(assignment_path)
+  checkmate::assert_flag(force_get_tests)
+  checkmate::assert_flag(quiet)
   
-  if(!missing(assignment_path)) {
+  if(!is.null(assignment_path)) {
     old_warn_opt <- options(warn = 2)
     set_assgn_result <- try(suppressMessages(set_assignment(assignment_path)), silent = TRUE)
     options(warn = old_warn_opt$warn)
     if(is(set_assgn_result, "try-error"))
       stop(set_assgn_result[1])
   }
-  if(missing(reporter)) reporter <- get_mark_my_reporter()
-  
-  assert_function_arguments_in_API(
-    tasks = tasks, mark_file = mark_file, lab_file = assignment_path,
-    force_get_tests = force_get_tests, quiet = quiet, reporter = reporter)
-  
   
   get_tests(tasks = tasks, force_get_tests = force_get_tests)
-  test_results <- run_test_suite("mark_my_file", tasks, mark_file, quiet, reporter = reporter)
+  test_results <- run_test_suite("mark_my_file", tasks, mark_file, quiet)
   test_results_df <- as.data.frame(test_results)
   if(!any(test_results_df$error) & sum(test_results_df$failed) == 0 & is.null(tasks) & !quiet) cheer()
   check_existance_tasks(tasks = tasks)
