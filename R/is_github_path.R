ab3aae7250c67ca780a92b199ab85a86e3f72141
Måns Magnusson <mons.magnusson@gmail.com>
2017-12-28 11:49:42 +0100

    Small fixes

--- R/is_github_path.R
+++ R/is_github_path.R
@@ -8,7 +8,10 @@ is_github_path <- function(path){
   is_raw <- grepl("//raw\\.githubusercontent\\.com", path)
   is_http <- grepl("//github\\.com", path)
   is_api <- grepl("//api\\.github\\.com", path)
+  split_path <- strsplit(path, "/")[[1]]
+  is_contents_api_path <- any(grepl("^contents$", split_path)) & is_api
+  is_git_api_path <- any(grepl("^git$", split_path)) & is_api
   is_github <- is_raw | is_http | is_api 
-  if(is_github) names(is_github) <- c("raw", "http", "api")[c(is_raw, is_http, is_api)]
+  if(is_github) names(is_github) <- c("raw", "http", "api_contents", "api_git")[c(is_raw, is_http, is_contents_api_path, is_git_api_path)]
   is_github
 }
--- tests/testthat/test-is_github_path.R
+++ tests/testthat/test-is_github_path.R
@@ -7,7 +7,8 @@ test_that(desc="expect_function_self_contained()",{
   expect_true(is_github_path("https://raw.githubusercontent.com/octokit/octokit.rb/master/README.md"))
   expect_false(is_github_path("https://google/octokit/octokit.rb/blob/github/README.md"))
 
-  expect_names(names(is_github_path("https://api.github.com/repos/octokit/octokit.rb/contents/README.md")), identical.to = "api")
+  expect_names(names(is_github_path("https://api.github.com/repos/octokit/octokit.rb/contents/README.md")), identical.to = "api_contents")
+  expect_names(names(is_github_path("https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1")), identical.to = "api_git")
   expect_names(names(is_github_path("https://github.com/octokit/octokit.rb/blob/master/README.md")), identical.to = "http")
   expect_names(names(is_github_path("https://raw.githubusercontent.com/octokit/octokit.rb/master/README.md")), identical.to = "raw")
   expect_null(names(is_github_path("https://google/octokit/octokit.rb/blob/github/README.md")))
