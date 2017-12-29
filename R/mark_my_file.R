5dec25f12e164ce0b7ef935a56d51c3505a82d0c
Måns Magnusson <mons.magnusson@gmail.com>
2017-12-29 15:38:16 +0100

    Small bugg fix

--- R/mark_my_file.R
+++ R/mark_my_file.R
@@ -12,7 +12,7 @@
 #' @param assignment_path
 #'   Assignment file to set before marking the assignment (url or local path).
 #' @param force_get_tests
-#'   Force download of test files before marking of assignments. Default is FALSE.
+#'   Argument is deprecated, use \code{set_assignment()} instead.
 #' @param quiet
 #'   Should test be run without output?
 #' @param ... further arguments sent to \code{test_dir()}.
@@ -31,6 +31,10 @@ mark_my_file <- function(tasks = NULL, mark_file=file.choose(), assignment_path 
   checkmate::assert_flag(force_get_tests)
   checkmate::assert_flag(quiet)
   
+  if(force_get_tests){
+    .Deprecated("set_assignment()", old = "force_get_tests")
+  }
+  
   if(!is.null(assignment_path)) {
     old_warn_opt <- options(warn = 2)
     set_assgn_result <- try(suppressMessages(set_assignment(assignment_path)), silent = TRUE)
@@ -39,11 +43,18 @@ mark_my_file <- function(tasks = NULL, mark_file=file.choose(), assignment_path 
       stop(set_assgn_result[1])
   }
   
-  get_tests(tasks = tasks, force_get_tests = force_get_tests)
-  test_results <- run_test_suite("mark_my_file", tasks, mark_file, quiet)
+  if(quiet){
+    capture_output(test_results <- suppressMessages(run_test_suite(caller = "mark_my_file", tasks, mark_file, quiet, ...)))
+  } else {
+    test_results <- run_test_suite(caller = "mark_my_file", tasks, mark_file, quiet, ...)
+  }
+
   test_results_df <- as.data.frame(test_results)
   if(!any(test_results_df$error) & sum(test_results_df$failed) == 0 & is.null(tasks) & !quiet) cheer()
   check_existance_tasks(tasks = tasks)
+  
+  if(!is.null(assignment_path)) remove_assignment()
+  
   return(invisible(test_results))
 }
 
