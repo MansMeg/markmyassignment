a5e2219e0e889ecc12133464a1fc8d5dc19f3a93
Måns Magnusson <mons.magnusson@gmail.com>
2017-12-29 14:22:06 +0100

    Bug fix

--- R/assertion_funcions.R
+++ R/assertion_funcions.R
@@ -14,7 +14,7 @@
 #' @keywords internal
 #' 
 check_existance_tasks <- function(tasks, path = NULL){
-  res <- read_assignment_yml(path = path)
+  res <- read_assignment_yml(path)
   if(!all(tasks %in% names(res$tasks))){
     warning(paste("The following tasks do not exist:", paste(
       tasks[!(tasks %in% names(res$tasks))], collapse = ", ")))
