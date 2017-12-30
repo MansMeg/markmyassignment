a36908fc89989f65b802554049a0e2e44486f860
Måns Magnusson <mons.magnusson@gmail.com>
2017-12-30 14:00:37 +0100

    Adding teacher vignette

---
+++ vignettes/markmyassignment-teacher.R
@@ -0,0 +1,21 @@
+## ---- message=FALSE, eval=FALSE------------------------------------------
+#  install.packages("markmyassignment")
+
+## ---- message=FALSE, eval=FALSE------------------------------------------
+#  help(package = "markmyassignment")
+
+## ---- message=FALSE, eval=FALSE------------------------------------------
+#  vignette("markmyassignment")
+
+## ---- message=FALSE, eval=FALSE------------------------------------------
+#  set_assignment("https://github.com/MansMeg/markmyassignment/blob/master/inst/extdata/assignment_template.yml")
+
+## ---- message=FALSE, eval=FALSE------------------------------------------
+#  vignette("markmyassignment")
+
+## ---- message=FALSE, eval=FALSE------------------------------------------
+#  mark_my_assignment(reporter = "summary")
+
+## ---- message=FALSE, eval=FALSE------------------------------------------
+#  set_assignment("<github private repo url>", httr::authenticate("<Github_Personal_Access_Token>", ""))
+
