782ae63712cbe12aa5b7d6137963b559c19d7ee5
Måns Magnusson <mons.magnusson@gmail.com>
2025-11-22 16:53:15 +0100

    Updates

---
+++ R/_PACKAGE.R
@@ -0,0 +1,9 @@
+#' markmyassignment: Tools to work with student assignments
+#'
+#' Automated testing of R assignments.
+#'
+#' @details
+#' Automatic marking of R assignments for students and teachers based on testthat test suites.
+#'
+#' @keywords internal
+"_PACKAGE"
